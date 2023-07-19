67f111c977d4ae410664a044da63a34fe972be36
2022phyro <phyrokelstein2@gmail.com>
2023-07-19 20:22:50 +0100

    Debugging this question

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,8 +3,12 @@
 import re
 import sys
 
-regex = r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+ma1 = r'^(\d+\.\d+\.\d+\.\d+) -'
+ma2 = r' \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\]'
+ma3 = r' "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
+regex = ma1 + ma2 + ma3
+codes = [200, 301, 400, 401, 403, 404, 405, 500]
+ans = [0, 0, 0, 0, 0, 0, 0, 0]
 
 count = 0
 
@@ -14,16 +18,17 @@ try:
     for line in sys.stdin:
         if count % 10 == 0 and count != 0:
             print(f"File size: {file_size}")
-            for k, v in codes.items():
+            for k, v in zip(codes, ans):
                 print(f"{k}: {v}")
         matches = re.match(regex, line)
         if matches:
             x = int(matches.group(3))
-            if x in codes.keys():
-                codes[x] += 1
+            if x in codes:
+                y = codes.index(x)
+                ans[y] += 1
                 file_size += int(matches.group(4))
         count += 1
 except KeyboardInterrupt:
     print(f"File size: {file_size}")
-    for k, v in codes.items():
+    for k, v in zip(codes, ans):
         print(f"{k}: {v}")
