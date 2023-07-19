7425072e2f56c280495715ca92f8b12082b6fd8d
2022phyro <phyrokelstein2@gmail.com>
2023-07-19 20:37:42 +0000

    Tested it on python 3.4

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-"""PArsing log files"""
+"""Parsing log files with python"""
+
 import re
 import sys
 
-ma1 = r'^(\d+\.\d+\.\d+\.\d+) -'
-ma2 = r' \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\]'
-ma3 = r' "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
+ma1 = r'^(\d+\.\d+\.\d+\.\d+) -' 
+ma2 = r' \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\]' 
+ma3 = r' "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$' 
 regex = ma1 + ma2 + ma3
 codes = [200, 301, 400, 401, 403, 404, 405, 500]
-ans = [0, 0, 0, 0, 0, 0, 0, 0]
 
-count = 0
 
-file_size = 0
+def printf(codes, fsize):
+	"""Prints in the required format"""
+	print("File size: {}".format(fsize))
+	for k, v in sorted(codes.items()):
+		print("{}: {}".format(k, v))
 
-try:
-    for line in sys.stdin:
-        if count % 10 == 0 and count != 0:
-            print(f"File size: {file_size}")
-            for k, v in zip(codes, ans):
-                print(f"{k}: {v}")
-        matches = re.match(regex, line)
-        if matches:
-            x = int(matches.group(3))
-            if x in codes:
-                y = codes.index(x)
-                ans[y] += 1
-                file_size += int(matches.group(4))
-        count += 1
-except KeyboardInterrupt:
-    print(f"File size: {file_size}")
-    for k, v in zip(codes, ans):
-        print(f"{k}: {v}")
+if __name__ == "__main__":
+	res = {}
+	fsize = 0
+	try:
+		for i, line in enumerate(sys.stdin, 1):
+			matches = re.match(regex, line)
+			if matches:
+				x = int(matches.group(3))
+				if x in res:
+					res[x] += 1
+				elif x in codes:
+					res[x] = 1
+					codes.remove(x)
+				fsize += int(matches.group(4))
+			if i % 10 == 0:
+				printf(res, fsize)
+	except KeyboardInterrupt:
+		printf(res, fsize)
