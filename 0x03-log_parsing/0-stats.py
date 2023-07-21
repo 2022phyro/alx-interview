f36add9d7605a5866270f8bb4737d133a39544a0
2022phyro <phyrokelstein2@gmail.com>
2023-07-21 11:33:58 +0000

    Debugging and fixing pycodestyle errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,25 +5,24 @@ import re
 import sys
 
 
-
 def printf(codes, fsize):
-	"""Prints in the required format"""
-	print("File size: {}".format(fsize))
-	for k, v in sorted(codes.items()):
-		if v != 0:
-			print("{}: {}".format(k, v))
+    """Prints in the required format"""
+    print("File size: {}".format(fsize))
+    for k, v in sorted(codes.items()):
+    	if v != 0:
+    		print("{}: {}".format(k, v))
 fsize = 0
 codes = {'200': 0, '301': 0, '400': 0,
-		'401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    	'401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 try:
-	for i, line in enumerate(sys.stdin, 1):
-		vals = line.split()
-		if len(vals) > 3:
-			code = vals[-2]
-			if code in codes:
-				codes[code] += 1
-			fsize += int(vals[-1])
-			if i % 10 == 0:
-				printf(codes, fsize)
+    for i, line in enumerate(sys.stdin, 1):
+    	vals = line.split()
+    	if len(vals) > 3:
+    		code = vals[-2]
+    		if code in codes:
+    			codes[code] += 1
+    		fsize += int(vals[-1])
+    		if i % 10 == 0:
+    			printf(codes, fsize)
 finally:
-	printf(codes, fsize)
+    printf(codes, fsize)
