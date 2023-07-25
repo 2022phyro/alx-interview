f0035c2b24157c7ab573d1702cf3e6b23e1dd350
2022phyro <phyrokelstein2@gmail.com>
2023-07-25 14:50:56 +0100

    Debugging

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,4 +8,20 @@ def validUTF8(data):
     Args:
         data (int): a sequence representing bytes
     """
-    return all(x >= 0 and x < 256 for x in data)
+    i = 0
+    while i < len(data):
+        # Check for 1-byte character (0xxxxxxx)
+        if (data[i] & 0b10000000) == 0:
+            i += 1
+        # Check for 2-byte character (110xxxxx 10xxxxxx)
+        elif i + 1 < len(data) and (data[i] & 0b11100000) == 0b11000000 and (data[i + 1] & 0b11000000) == 0b10000000:
+            i += 2
+        # Check for 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
+        elif i + 2 < len(data) and (data[i] & 0b11110000) == 0b11100000 and (data[i + 1] & 0b11000000) == 0b10000000 and (data[i + 2] & 0b11000000) == 0b10000000:
+            i += 3
+        # Check for 4-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
+        elif i + 3 < len(data) and (data[i] & 0b11111000) == 0b11110000 and (data[i + 1] & 0b11000000) == 0b10000000 and (data[i + 2] & 0b11000000) == 0b10000000 and (data[i + 3] & 0b11000000) == 0b10000000:
+            i += 4
+        else:
+            return False
+    return True
