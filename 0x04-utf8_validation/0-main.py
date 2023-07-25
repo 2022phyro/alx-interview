838fdb480392cd360e9c1455ef6476ce717bd546
2022phyro <phyrokelstein2@gmail.com>
2023-07-25 15:26:28 +0100

    Still trying to understand this

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -3,6 +3,7 @@
 Main file for testing
 """
 
+
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
 data = [65]
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,26 +2,34 @@
 """This file attempts to check if a string is a valid UTF8 encoding
 """
 
+
 def validUTF8(data):
     """Checks if the numbers in the data is a valid utf8 encoding
 
     Args:
         data (int): a sequence representing bytes
     """
-    i = 0
-    while i < len(data):
-        # Check for 1-byte character (0xxxxxxx)
-        if (data[i] & 0b10000000) == 0:
-            i += 1
-        # Check for 2-byte character (110xxxxx 10xxxxxx)
-        elif i + 1 < len(data) and (data[i] & 0b11100000) == 0b11000000 and (data[i + 1] & 0b11000000) == 0b10000000:
-            i += 2
-        # Check for 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
-        elif i + 2 < len(data) and (data[i] & 0b11110000) == 0b11100000 and (data[i + 1] & 0b11000000) == 0b10000000 and (data[i + 2] & 0b11000000) == 0b10000000:
-            i += 3
-        # Check for 4-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
-        elif i + 3 < len(data) and (data[i] & 0b11111000) == 0b11110000 and (data[i + 1] & 0b11000000) == 0b10000000 and (data[i + 2] & 0b11000000) == 0b10000000 and (data[i + 3] & 0b11000000) == 0b10000000:
-            i += 4
+    num_bytes_to_process = 0
+
+    for byte in data:
+        # Check if it's a continuation byte (starts with 10)
+        if byte & 0xC0 == 0x80:
+            if num_bytes_to_process == 0:
+                return False
+            num_bytes_to_process -= 1
         else:
-            return False
-    return True
+            # Count the number of bytes to process based on the first byte
+            if num_bytes_to_process > 0:
+                return False
+            if byte & 0x80 == 0:
+                num_bytes_to_process = 0
+            elif byte & 0xE0 == 0xC0:
+                num_bytes_to_process = 1
+            elif byte & 0xF0 == 0xE0:
+                num_bytes_to_process = 2
+            elif byte & 0xF8 == 0xF0:
+                num_bytes_to_process = 3
+            else:
+                return False
+
+    return num_bytes_to_process == 0
