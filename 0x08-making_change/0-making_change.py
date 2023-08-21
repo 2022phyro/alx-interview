1dc8acc752581baace40f794a2fc2b1375d53d52
2022phyro <phyrokelstein2@gmail.com>
2023-08-21 15:58:05 +0100

    Greedy algorithms to get change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Another application of greedy algorithm"""
+
+
+def makeChange(coins: list, total):
+    """This function uses a greedy algorithm
+    to calculate the number of change needed for
+    for a larger denomination"""
+    result = 0
+    if total <= 0:
+        return 0
+    while True:
+        x = max(coins)
+        while x <= total:
+            total -= x
+            result += 1
+        coins.remove(x)
+        if total == 0:
+            return result
+        if coins == []:
+            return -1
