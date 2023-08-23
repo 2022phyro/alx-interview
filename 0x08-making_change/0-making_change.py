d78ed8cab444a2f3c8fa9f2d50b8cbd5e6d80210
2022phyro <phyrokelstein2@gmail.com>
2023-08-23 09:02:05 +0100

    Debugging fixed it to return -1 if there are no coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,8 +7,10 @@ def makeChange(coins: list, total):
     to calculate the number of change needed for
     for a larger denomination"""
     result = 0
-    if total <= 0 or coins == []:
+    if total <= 0:
         return 0
+    if not coins:
+        return -1
     while True:
         x = max(coins)
         while x <= total:
